775cf7656ac942d47e57d342356a13651e93edce
itsdeeksha15 <dee.ksha015180@gmail.com>
2026-03-01 20:39:06 +0530

    Add files via upload
    UI enhancement and popup feedback

--- script.js
+++ script.js
@@ -2,42 +2,66 @@ let web3;
 let contract;
 let account;
 
-const contractAddress = "0xb0782271f6Cb1a26EF01c0197a16cA2687FFB5Ee";
+const contractAddress = "PASTE_YOUR_DEPLOYED_CONTRACT_ADDRESS";
 
 const abi = [
-    {
-        "inputs": [],
-        "name": "giveGoodFeedback",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "giveAverageFeedback",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    }
+  {
+    "inputs": [],
+    "name": "giveGoodFeedback",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "giveAverageFeedback",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
 ];
 
+function showPopup(message) {
+    const popup = document.getElementById("popup");
+    popup.innerText = message;
+    popup.style.display = "block";
+
+    setTimeout(() => {
+        popup.style.display = "none";
+    }, 2500);
+}
+
 async function connectWallet() {
-    if (window.ethereum) {
-        web3 = new Web3(window.ethereum);
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
-        const accounts = await web3.eth.getAccounts();
-        account = accounts[0];
-        document.getElementById("account").innerText = "Connected: " + account;
-        contract = new web3.eth.Contract(abi, contractAddress);
+    if (!window.ethereum) {
+        showPopup("MetaMask not found");
+        return;
     }
+
+    web3 = new Web3(window.ethereum);
+    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+    account = accounts[0];
+
+    contract = new web3.eth.Contract(abi, contractAddress);
+
+    showPopup("Wallet connected successfully");
 }
 
 async function goodFeedback() {
+    if (!contract) {
+        showPopup("Connect wallet first");
+        return;
+    }
+
     await contract.methods.giveGoodFeedback().send({ from: account });
-    alert("Thank you for your feedback!");
+    showPopup("Thank you for your feedback 👍");
 }
 
 async function averageFeedback() {
+    if (!contract) {
+        showPopup("Connect wallet first");
+        return;
+    }
+
     await contract.methods.giveAverageFeedback().send({ from: account });
-    alert("Thank you for your feedback!");
+    showPopup("Feedback recorded 😐");
 }
